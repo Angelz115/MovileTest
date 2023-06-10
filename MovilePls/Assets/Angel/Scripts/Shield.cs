a50952d415030f000cfa997ad5f977dffd5e0e9d
Angelz115 <66693671+Angelz115@users.noreply.github.com>
2023-06-09 23:30:34 -0400

    avance

--- MovilePls/Assets/Angel/Scripts/Shield.cs
+++ MovilePls/Assets/Angel/Scripts/Shield.cs
@@ -7,6 +7,7 @@ public class Shield : MonoBehaviour
     public GameObject controller;
     public GameObject ShieldObj;
     Vector3 rotation;
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,8 +17,8 @@ public class Shield : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("X rot : "+ controller.transform.position.x + " Y rot : " + controller.transform.position.y);
-        //rotation = new Vector3(controller.)
-        //ShieldObj.transform.RotateAround(transform.position);
+        //Debug.Log("X rot : "+ controller.transform.position.x + " Y rot : " + controller.transform.position.y);
+        rotation = new Vector3(controller.transform.position.x, controller.transform.position.y,0);
+        ShieldObj.transform.RotateAround(transform.position, rotation, speed);
     }
 }
