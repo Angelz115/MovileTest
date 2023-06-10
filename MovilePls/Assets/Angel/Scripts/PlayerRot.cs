14d79f817497031bfede76630821d7da5bf2076e
Angelz115 <66693671+Angelz115@users.noreply.github.com>
2023-06-10 15:39:47 -0400

    Rotacion Jugador
    La rotación esta lista, se removió el script del escudo porque se rota al jugador

--- MovilePls/Assets/Angel/Scripts/PlayerRot.cs
+++ MovilePls/Assets/Angel/Scripts/PlayerRot.cs
@@ -5,31 +5,19 @@ using UnityEngine;
 public class PlayerRot : MonoBehaviour
 {
     public GameObject handle;
-    public float speed = 5;
     public Vector2 rotHandle;
-    public Vector2 compare = new Vector2(127,0); 
-    public Rigidbody2D rb;
-    
 
     // Update is called once per frame
     void Update()
     {
         
-        //rb.MoveRotation();
         rotHandle = new Vector2(handle.GetComponent<RectTransform>().anchoredPosition.x, handle.GetComponent<RectTransform>().anchoredPosition.y);
-        float Up = rotHandle.x * rotHandle.y;
-        float Udown = Mathf.Sqrt(Mathf.Pow(127, 2) + Mathf.Sqrt(Mathf.Pow(0, 2)));
-        float Vdown = Mathf.Sqrt(Mathf.Pow(rotHandle.y, 2) + Mathf.Pow(rotHandle.y, 2));
-        float Down = Udown * Vdown;
-        float angle =Mathf.Pow( Mathf.Cos(Up / Down),-1 );
-        
-        if (Up >= 1300)
+        if (rotHandle.x != 0 || rotHandle .y != 0)
         {
-            Debug.Log(Down);
-            Debug.Log("Ver si : " + Mathf.Sqrt(Mathf.Pow(0, 2)));
-            //Debug.Log(Mathf.Pow(speed,2));
-
+            Vector2 direction = new Vector2(rotHandle.x - transform.position.x, rotHandle.y - transform.position.y);
+            transform.up = direction;
         }
-        //transform.Rotate(handle.transform.position * speed);
+        
+        
     }
 }
