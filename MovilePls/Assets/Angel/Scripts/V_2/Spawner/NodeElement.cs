31dbc9772f2cfb09664e5e604e32e31ac61915a9
Angelz115 <66693671+Angelz115@users.noreply.github.com>
2023-06-24 12:55:02 -0400

    Mover carpetas

---
+++ MovilePls/Assets/Angel/Scripts/V_2/Spawner/NodeElement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Entity {Point, Element}
+public class NodeElement : MonoBehaviour
+{
+    public Entity entity;
+    public Vector2 position;
+    public Vector2 target;
+    public int value;
+
+    public NodeElement(Entity ent, Vector2 pos, Vector2 tar, int val) 
+    {
+        this.entity = ent;
+        this.position = pos;
+        this.target = tar;
+        this.value = val;
+
+        
+    }
+}
---
+++ MovilePls/Assets/Angel/Scripts/V_2/Spawner/SpawnerV2.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerV2 : MonoBehaviour
+{
+    public List<NodeElement> Elements = new List<NodeElement>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
